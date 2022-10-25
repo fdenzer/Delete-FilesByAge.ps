e9d90c0d48d346d578f58d8ce0c2f06630fcb83a
F. Denzer <1089372+fdenzer@users.noreply.github.com>
2022-10-25 17:00:23 +0200

    sample usage

--- Delete-FilesByAge.ps1
+++ Delete-FilesByAge.ps1
@@ -417,6 +417,13 @@ $fileParams
 Write-Host ""
 [datetime]$oldDate = (Get-Date).AddDays(-$daysToKeep)
 
+# sample usage:
+#.\Delete-FilesByAge.ps1 newdir [] [] 1 outdir
+# make files and folders old:
+# $file = Get-Item newdir/newfile
+# $file.LastWriteTime = (Get-Date -Day 1)
+# $folder = Get-Item newdir/newdir
+# $folder.LastWriteTime = (Get-Date -Day 1)
 
 # this is where the magic happens:
 
