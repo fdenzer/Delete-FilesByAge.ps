45b51f44c21341eabd3c0ed0665de6f93619647e
F. Denzer <1089372+fdenzer@users.noreply.github.com>
2022-10-26 14:07:39 +0200

    docs

--- README.md
+++ README.md
@@ -1,16 +1,31 @@
-# make files and folders and set their date to be old:
+# Dear Wilo, first note that:
 
-> See: create_testenv.ps1
+1. The script works in recursive mode! This might be dangerous if you have junctions or other hard-links to folders in your directory structure!
 
-# sample usage:
+2. Please overwrite the concrete folders which are searched, and set a reasonable maximum days of age before running.
 
-> See: run_on_working_dir.ps1
+    - Either do this on a command line, using the following three lines to configure the script, then run it with the last line (after the comment).
+
+    - Or modify the run_on_working_dir.ps1 script file, which contains these three configuration lines as well. Then you may execute the script using always the same parameters.
 
 ```powershell
-$input_directories = 'working-directory' #, ...
-$days = 1
-$log_directory = 'outdir'
+$input_directories = 'Ordner1', 'Ordner zwo'
+$days = 180
+$log_directory = 'log_directory'  # locally, or use a path like "C:\Users\Administrator\Desktop\delete_script_logging" or "C:\Benutzer\Erika Mustermann\Desktop\log_verzeichnis". Whitespaces are allowed in any input string.
 
+# actually run script with these parameters. Optionally, files to delete (*.*) or exceptions from deletion can be defined as parameters two and three
 .\Delete-FilesByAge.ps1 $input_directories "*.*" "" $days $log_directory -Recurse
 ```
 
+
+# To test the script...
+
+* we provide a create test environment script to create files and folders and set their date to be old.
+
+* See: create_testenv.ps1
+
+* Then take a look into 
+
+> run_on_working_dir.ps1
+
+and run it to clean up these files.
--- run_on_working_dir.ps1
+++ run_on_working_dir.ps1
@@ -1,15 +1,21 @@
+# Dear Wilo, please overwrite the concrete folders which are searched, and set a reasonable maximum days of age before running:
+
 # sample usage for cleanup after create_testenv script:
-#$input_directories = 'working-directory', 'another directory with whitespace'
+$input_directories = 'working-directory', 'another directory with whitespace'
 
+# Imaginary input directories given below, to show that they could contain numbers or whitespace. These are valid input folders.
+#$input_directories = 'Ordner1', 'Ordner zwo'
 
-
-# Dear Wilo, please overwrite these folders, and maximum days of age:
-$input_directories = 'Ordner1', 'Ordner zwo'
 $days = 180
-$log_directory = 'log_directory'  # locally, or use a path like "C:\Users\Administrator\Desktop\delete_script_logging" or "C:\Benutzer\Erika Mustermann\Desktop\log_verzeichnis". Whitespaces are allowed in any input string.
+
+# Next up, we locally use a folder which will be created if necessary for logging output,
+# or we may opt to use a path like "C:\Users\Administrator\Desktop\delete_script_logging" or "C:\Benutzer\Erika Mustermann\Desktop\log_verzeichnis".
+# Whitespaces are allowed in any input string.
+$log_directory = 'log_directory' 
 
 
 
-# actually run script with these parameters. Optionally, files to delete (*.*) or exceptions from deletion can be defined as parameters two and three
+# The next line actually runs script with these parameters.
+# Optionally, files to delete (*.*) or exceptions from deletion can be defined as parameters two and three
 # Works in recursive mode! This might be dangerous if you have junctions or other hard-links to folders in your directory structure!
 .\Delete-FilesByAge.ps1 $input_directories "*.*" "" $days $log_directory -Recurse
